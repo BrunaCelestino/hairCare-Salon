e660b8bdcc45b8d0e9f015f994c8dd0b6ad42628
BrunaCelestino <brunaccelestino@gmail.com>
2022-08-15 14:18:06 -0300

    fix: node modules

---
+++ server/src/routes/indexRoutes.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  res.status(200).json({
+    title: 'HairCare-Salon',
+    version: '1.0.0',
+    mensagem:
+      'HairCare-Salon API is a system for scheduling appointments in any HairCare-Salon chain unit.',
+  });
+});
+
+module.exports = routes;
