60fa6a7ae7dceab682aef3f0749aabffdae7ed4f
BrunaCelestino <brunaccelestino@gmail.com>
2022-08-16 23:49:16 -0300

    feat: add function paths

--- server/src/app.js
+++ server/src/app.js
@@ -2,24 +2,32 @@ const express = require('express');
 
 const app = express();
 const cors = require('cors');
-const swaggerUI = require('swagger-ui-express');
+// const swaggerUI = require('swagger-ui-express');
 
 app.use(cors());
 
 require('dotenv-safe').config();
 
-const db = require('./database/mongoConfig');
+const db = require('./database/postgreSQLconfig');
 
 db.connect();
 
-const swaggerDocument = require('./swagger.json');
+// const swaggerDocument = require('./swagger.json');
 const index = require('./routes/indexRoutes');
-const localAdminRoutes = require('./routes/localAdminRouter');
+const serviceRoutes = require('./routes/serviceRoutes');
+const unitRoutes = require('./routes/unitRoutes');
+const clientRoutes = require('./routes/clientRoutes');
+const professionalRoutes = require('./routes/professionalRoutes');
+const schedulelRoutes = require('./routes/scheduleRoutes');
 
 app.use(express.json());
 
 app.use('/', index);
-app.use('/admin', localAdminRoutes);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/service', serviceRoutes);
+app.use('/unit', unitRoutes);
+app.use('/client', clientRoutes);
+app.use('/professional', professionalRoutes);
+app.use('/schedule', schedulelRoutes);
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 module.exports = app;
